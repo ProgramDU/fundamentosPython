a75590391f8733b1169ed7884d94bc846e6f3035
ProgramDU <bernardinodaniela96@gmail.com>
2023-06-09 16:20:35 -0500

    02 variables

---
+++ mail.py
@@ -0,0 +1,9 @@
+# Funcion print
+print("Hola mundo")
+print("soy Dani")
+
+print("podemos enviar diferentes tipos de dato")
+print(12)
+print(12.23)
+print(true)
+print([])
---
+++ variables.py
@@ -0,0 +1,15 @@
+# pyton es No tipado
+
+name= "Daniela"
+Last_name = "Bernardino"
+age = 19
+height =1.68
+is_estudent = "true"
+group = ["3C"]
+
+# funcion type
+print (type(name))
+print (type (age))
+print (type (height))
+print (type (is_estudent))
+print (type(group))
