dca848722b121972cc11f8a56f64786656ed9c93
ProgramDU <bernardinodaniela96@gmail.com>
2023-06-09 16:54:15 -0500

    04 operadore

---
+++ operadores.py
@@ -0,0 +1,20 @@
+# operadores matematicos 
+# + - * %
+# / divicion float
+# // division entera 
+# ** potencia
+
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+# operadores para strings
+# + para contatenar
+# * para replicar 
+
+print("-------" * 4)
+print("hola " + "hola mundo")
