0877cfec0731b55e2ca1487caaefc1f1369077f2
ProgramDU <bernardinodaniela96@gmail.com>
2023-06-13 13:02:02 -0500

    07 strings

---
+++ strings.py
@@ -0,0 +1,24 @@
+my_string = " esTe eS Mi StrInG"
+
+# longitud de string 
+print (len(my_string))
+#mayusculas
+print(my_string.upper())
+#minusculas
+print(my_string.lower())
+# capitalice primera letra a Mayus
+print(my_string.capitalize())
+# eliminar espacios inicio fin
+print(my_string.strip())
+
+# indexing and slicing
+my_string = "hola 3C DSM"
+print(my_string[0])
+print(my_string[6])
+print(my_string[3])
+print(my_string[-1])
+print(my_string[-2])
+
+print(my_string[0:2])
+print(my_string[5:7])
+print(my_string[5:-1])
